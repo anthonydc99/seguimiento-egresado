fb6bf9528e6a13f205c268b53f0b46e41f57adbe
Anthony Diaz <anthony.diaz2@unmsm.edu.pe>
2019-05-30 21:27:00 -0500

    Front Formulario1 finalizado

--- src/Componentes/VistaDatosPersonales.js
+++ src/Componentes/VistaDatosPersonales.js
@@ -15,16 +15,59 @@ class VistaDatosPersonales extends React.Component {
 
     render() {
         return (
-            <div className="contenedor" style={{ height: window.innerHeight-90}}>
-                    <div className="">
-                        <h1>Datos Personales</h1>
-                        <h1>Datos Personales</h1><h1>Datos Personales</h1><h1>Datos Personales</h1><h1>Datos Personales</h1>
-                        <h1>Datos Personales</h1><h1>Datos Personales</h1><h1>Datos Personales</h1><h1>Datos Personales</h1>
-                        <h1>Datos Personales</h1><h1>Datos Personales</h1><h1>Datos Personales</h1>
-                        <h1>Datos Personales</h1><h1>Datos Personales</h1><h1>Datos Personales</h1>
-
+            <div className="contenedor" style={{ height: window.innerHeight - 90 }}>
+                <div className="">
+                    <h2 className="titulo">Datos Personales</h2>
+                    <div className="input-dato">
+                        <label className="label-dato">
+                            N° DE DOCUMENTO DE IDENTIDAD (DNI O CARNE DE EXTRANJERÍA):
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            APELLIDO PATERNO:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            APELLIDO MATERNO:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            NOMBRES:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            FECHA DE NACIMIENTO:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            DOMICILIO ACTUAL:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            DISTRITO:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            N° TELÉFONO FIJO:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            N° TELÉFONO CELULAR:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            CORREO ELECTRÓNICO PERSONAL:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <label className="label-dato">
+                            CORREO ELECTRÓNICO LABORAL:
+                            <input type="text" name="name" required/>
+                        </label>
+                        <input type="submit" value="Enviar" className="btn right" required/>
                     </div>
+
                 </div>
+            </div>
         );
     }
 }
